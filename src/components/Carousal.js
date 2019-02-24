10411eacce9e8f2ac23437259082fbcf370172ba
contactyash <kanusdlp@gmail.com>
2019-02-24 21:21:47 +0530

    minor

--- src/components/Carousal.js
+++ src/components/Carousal.js
@@ -27,14 +27,14 @@ export default function ImageSlider({ imagesArr, index, direction, fadeOn }) {
   const forwardTransitions = useTransition(index, p => p, {
     from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
     enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-100%,0,0)' },
     config: (item, state) =>
       state === 'leave' ? { duration: 0 } : config.default,
   })
   const backwordTransitions = useTransition(index, p => p, {
     from: { opacity: 0, transform: 'translate3d(-100%,0,0)' },
     enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-    leave: { opacity: 0, transform: 'translate3d(50%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(100%,0,0)' },
     config: (item, state) =>
       state === 'leave' ? { duration: 0 } : config.default,
   })
